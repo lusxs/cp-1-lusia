85d922208583b5354148a94d3e226d4cc9c9c1e4
lusxs <lusiakaparang8@gmail.com>
2023-03-09 11:28:07 +0800

    first commit

---
+++ src/main/kotlin/com/lusia/kotlin/App.kt
@@ -0,0 +1,43 @@
+package com.lusia.kotlin
+
+fun isPrimeNumber( x: Int,i: Int =2): Boolean {
+
+//    for (i in 2..number / 2) {
+//        if (number % i == 0) {
+//            return false
+//        }
+//    }
+//    return true
+
+    if (x < 2)
+        return false
+    if (i * i > x)
+        return true
+    if (x % i == 0)
+        return false
+    return isPrimeNumber(x, i + 1)
+
+}
+
+fun main(a: Array<String>) {
+
+    val test = readLine()!!.toInt()
+    repeat(test) {
+        val i = readLine()!!.toInt()
+        val result = if(isPrimeNumber(i)) "Prime Number" else "Not Prime"
+        println("Case #${it+1}: $result ")
+
+
+    }
+
+//    println("Is 5 prime number? " + isPrimeNumber(5))
+//    println("Is 6 prime number? " + isPrimeNumber(6))
+//    println("Is 7 prime number? " + isPrimeNumber(7))
+
+}
+
+
+
+
+
+
